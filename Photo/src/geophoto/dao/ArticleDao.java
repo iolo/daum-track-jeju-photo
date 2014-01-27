45fe85da83b142b60332ef185255e88c5be96e90
pop9310 <pop9310@31c9631b-4e61-c587-f4fa-cf6fc085d7c1>
2014-01-27 08:24:28 +0000

    안드에서 포스트 업로드시에 각각의 사용자 엑세스 토큰을 받아다가 포스팅 하도록 구현함.

--- Photo/src/geophoto/dao/ArticleDao.java
+++ Photo/src/geophoto/dao/ArticleDao.java
@@ -2,6 +2,7 @@ package geophoto.dao;
 
 import geophoto.cmd.ArticleCommand;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -24,14 +25,37 @@ public class ArticleDao extends SqlMapClientDaoSupport {
 		getSqlMapClientTemplate().insert("article.insertArticle", cmd);
 	}
 
-	// public List<ArticleCommand> getArticleList(Map<String, Integer> map)
-	// throws Exception {
-	// return (List<ArticleCommand>)
-	// getSqlMapClientTemplate().queryForList("article.getArticleList", map);
-	// }
+	public ArticleCommand getArticle(String postId) throws Exception {
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("postId", postId);
+
+		return (ArticleCommand) getSqlMapClientTemplate().queryForObject("article.getArticle", map);
+	}
 
 	public List<ArticleCommand> getArticleList(Map<String, Integer> map, String orderby) throws Exception {
 		return (List<ArticleCommand>) getSqlMapClientTemplate().queryForList("article.getArticleList_".concat(orderby), map);
 	}
+
+	public void updateLikecnt(Map<String, String> map) throws Exception {
+		getSqlMapClientTemplate().update("article.updateLikeCount", map);
+	}
+
+	// getSqlMapClientTemplate().execute(new SqlMapClientCallback() {
+	//
+	// public Object doInSqlMapClient(SqlMapExecutor executor) throws
+	// SQLException {
+	//
+	// executor.startBatch();
+	//
+	// Iterator<BookInfoBean> it = list.iterator();
+	// while (it.hasNext()) {
+	// BookInfoBean bean = it.next();
+	// executor.update("kr.lancerme.insertBookInfo", bean);
+	// }
+	//
+	// executor.executeBatch();
+	// return null;
+	// }
+	// });
 
 }
